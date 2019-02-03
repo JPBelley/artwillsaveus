7ab236e6158b8edad67553aa48da869cc83bf1d1
Unknown <belleyjeanphilippe@gmail.com>
2019-02-03 13:19:04 -0500

    History test

--- src/js/home.js
+++ src/js/home.js
@@ -1,11 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-  var headline = document.getElementById("headline");
-  var string = headline.innerText;
+var spanText = function spanText(text) {
+  var string = text.innerText;
   var spaned = '';
   for (var i = 0; i < string.length; i++) {
     if(string.substring(i, i + 1) === ' ') spaned += string.substring(i, i + 1);
     else spaned += '<span>' + string.substring(i, i + 1) + '</span>';
   }
-  headline.innerHTML = spaned;
+  text.innerHTML = spaned;
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var headline = document.getElementById("headline");
+
+  if(headline) {
+    spanText(headline);
+  }
+
 }, false);
