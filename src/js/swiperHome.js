11affb96fc365a0276075953ff29cf05b17dc447
Jean-Philippe Belley <jean-philippe.belley@cossette.com>
2019-07-13 13:13:50 -0400

    Divided the picture in 16 square with overflow hidden

--- src/js/swiperHome.js
+++ src/js/swiperHome.js
@@ -1,6 +1,47 @@
-var swiper = new Swiper('.swiper-container', {
+var interleaveOffset = 0.5;
+
+var swiperOptions = {
+  loop: true,
+  speed: 1000,
+  grabCursor: true,
+  watchSlidesProgress: true,
+  effect: 'fade',
+  mousewheelControl: true,
+  keyboardControl: true,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
   },
-});
+  on: {
+    progress: function () {
+      var swiper = this;
+      for (var i = 0; i < swiper.slides.length; i++) {
+        var slideProgress = swiper.slides[i].progress;
+        var innerOffset = swiper.width * interleaveOffset;
+        var innerTranslate = slideProgress * innerOffset;
+        console.log(slideProgress);
+        
+        swiper.slides[i].querySelector(".headline").setAttribute(
+          "style", "transform: translate3d(" + innerTranslate + "px, 0, 0);");
+        // swiper.slides[i].querySelector(".single-square").setAttribute(
+        //   "style", "transform: scale(" + innerTranslate + ")");
+      }
+    },
+    touchStart: function () {
+      var swiper = this;
+      for (var i = 0; i < swiper.slides.length; i++) {
+        swiper.slides[i].style.transition = "";
+      }
+    },
+    setTransition: function (speed) {
+      var swiper = this;
+      for (var i = 0; i < swiper.slides.length; i++) {
+        swiper.slides[i].style.transition = speed + "ms";
+        swiper.slides[i].querySelector(".headline").style.transition =
+          speed + "ms";
+      }
+    }
+  }
+};
+
+var swiper = new Swiper(".swiper-container", swiperOptions);
