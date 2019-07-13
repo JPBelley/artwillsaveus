59a5cc0cdcd3ea876c44e7c594c2a621195184bb
Jean-Philippe Belley <jean-philippe.belley@cossette.com>
2019-07-13 14:05:32 -0400

    Carousel block transition

--- src/js/swiperHome.js
+++ src/js/swiperHome.js
@@ -1,8 +1,12 @@
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
 var interleaveOffset = 0.5;
 
 var swiperOptions = {
   loop: true,
-  speed: 1000,
+  speed: 2000,
   grabCursor: true,
   watchSlidesProgress: true,
   effect: 'fade',
@@ -19,12 +23,9 @@ var swiperOptions = {
         var slideProgress = swiper.slides[i].progress;
         var innerOffset = swiper.width * interleaveOffset;
         var innerTranslate = slideProgress * innerOffset;
-        console.log(slideProgress);
         
         swiper.slides[i].querySelector(".headline").setAttribute(
           "style", "transform: translate3d(" + innerTranslate + "px, 0, 0);");
-        // swiper.slides[i].querySelector(".single-square").setAttribute(
-        //   "style", "transform: scale(" + innerTranslate + ")");
       }
     },
     touchStart: function () {
@@ -35,10 +36,29 @@ var swiperOptions = {
     },
     setTransition: function (speed) {
       var swiper = this;
+      console.log(swiper);
       for (var i = 0; i < swiper.slides.length; i++) {
         swiper.slides[i].style.transition = speed + "ms";
         swiper.slides[i].querySelector(".headline").style.transition =
           speed + "ms";
+        swiper.slides[i].querySelectorAll(".single-square").forEach(function(square) {
+          square.style.transition =
+            getRandomArbitrary(0, 1) + "s";
+          square.style.opacity =
+            "1";
+        });
+      }
+    },
+    transitionStart:function (speed) {
+      var swiper = this;
+
+      for (var i = 0; i < swiper.slides.length; i++) {
+        if(!swiper.slides[i].classList.contains('swiper-slide-active')) {
+          swiper.slides[i].querySelectorAll(".single-square").forEach(function (square) {
+            square.style.opacity =
+              "0";
+          });
+        }
       }
     }
   }
